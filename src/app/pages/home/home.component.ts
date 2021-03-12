e1c163c63704d9fc150a255e07fb5d570d967ca4
bleauwonder <elyzableau.om@gmail.com>
2021-03-11 20:11:02 -0500

    add jarallax and restyle home and gallery

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import 'jarallax';
+declare var jarallax: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
   clickCounter: number = 0;
 
   constructor() { }
   ngOnInit() {
   }
 
-  countClick() {
-    this.clickCounter += 1;
+  ngAfterViewInit() {
+    jarallax(document.querySelectorAll('.jarallax'), {
+      speed: 0.2
+    });
   }
 
 }
